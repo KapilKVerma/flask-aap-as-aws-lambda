2eb34f380638377f5a01bea9fb04641d5a4280be
KapilKVerma <kapilkumaarverma@gmail.com>
2020-12-23 15:36:17 +1100

    Added post request

--- app.py
+++ app.py
@@ -3,20 +3,31 @@ from flask import Flask, render_template, request, jsonify
 # Initialize app
 app = Flask(__name__)
 
+items = [{
+    "id": 1,
+    "name": "product 1 name",
+    "description": "This is the first product description"
+}]
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
-        return jsonify({"message": "POST request received."})
+        item_data = request.get_json()
+        print(item_data)
+        items.append(item_data)
+        return jsonify({"items": items})
     if request.method == "GET":
         return jsonify({"message": "Magician server is running at port: 5000"})
 
 
-@app.route("/", methods=['PUT', 'DELETE'])
-def index_two():
+@app.route("/<int:id>", methods=['PUT', 'DELETE'])
+def index_two(id):
     if request.method == "PUT":
         return jsonify({"message": "PUT request received."})
     if request.method == "DELETE":
+        items.remove({'id': id})
+        print(id)
         return jsonify({"message": "DELETE request received."})
 
 
